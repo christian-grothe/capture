37d8a37086cc268bbb066633877d7dfe8f25cfff
Christian Grothe <christian.grothe@posteo.de>
2024-04-05 16:25:46 +0200

    adding semitones

--- capture_frontend/src/components/keyboard/Key.tsx
+++ capture_frontend/src/components/keyboard/Key.tsx
@@ -1,17 +1,18 @@
 import { useAppStore } from "../../store/useAppStore";
-import styles from "./keyboard.module.css"
+import styles from "./keyboard.module.css";
 
 interface Props {
-    midiNote: number;
+  midiNote: number;
 }
-
 export const Key = ({ midiNote }: Props) => {
-    const playNote = useAppStore(state => state.playNote)
-    const stopNote = useAppStore(state => state.stopNote)
-    return (
-        <div className={styles.key}
-            onMouseDown={() => playNote(midiNote)}
-            onMouseUp={() => stopNote(midiNote)}
-        ></div>
-    )
-}
+  const playNote = useAppStore((state) => state.playNote);
+  const stopNote = useAppStore((state) => state.stopNote);
+
+  return (
+    <div
+      className={styles.key}
+      onMouseDown={() => playNote(midiNote)}
+      onMouseUp={() => stopNote(midiNote)}
+    ></div>
+  );
+};
--- capture_frontend/src/store/useAppStore.ts
+++ capture_frontend/src/store/useAppStore.ts
@@ -1,61 +1,61 @@
 import { create } from "zustand";
 
 interface UseAppStore {
-	isInit: boolean;
-	audioCtx: AudioContext | undefined;
-	captureNode: AudioWorkletNode | undefined;
-	sourceNode: MediaStreamAudioSourceNode | undefined;
-	init: () => void;
-	record: () => void;
-	playNote: (midiNote: number) => void;
-	stopNote: (midiNote: number) => void;
+  isInit: boolean;
+  audioCtx: AudioContext | undefined;
+  captureNode: AudioWorkletNode | undefined;
+  sourceNode: MediaStreamAudioSourceNode | undefined;
+  init: () => void;
+  record: () => void;
+  playNote: (midiNote: number) => void;
+  stopNote: (midiNote: number) => void;
 }
 
 export const useAppStore = create<UseAppStore>((set, get) => {
-	return {
-		isInit: false,
-		audioCtx: undefined,
-		captureNode: undefined,
-		sourceNode: undefined,
-		init: async () => {
-			if (get().isInit) return;
-			const audioCtx = new AudioContext();
-			await audioCtx.audioWorklet.addModule("worklets/capture_processor.js");
-			const captureNode = new AudioWorkletNode(audioCtx, "capture", {
-				numberOfInputs: 1,
-				outputChannelCount: [2],
-			});
+  return {
+    isInit: false,
+    audioCtx: undefined,
+    captureNode: undefined,
+    sourceNode: undefined,
+    init: async () => {
+      if (get().isInit) return;
+      const audioCtx = new AudioContext();
+      await audioCtx.audioWorklet.addModule("worklets/capture_processor.js");
+      const captureNode = new AudioWorkletNode(audioCtx, "capture", {
+        numberOfInputs: 1,
+        outputChannelCount: [2],
+      });
 
-			const stream = await navigator.mediaDevices.getUserMedia({
-				audio: true,
-				video: false,
-			});
-			const sourceNode = audioCtx.createMediaStreamSource(stream);
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: false,
+      });
+      const sourceNode = audioCtx.createMediaStreamSource(stream);
 
-			sourceNode.connect(captureNode);
-			captureNode.connect(audioCtx.destination);
+      sourceNode.connect(captureNode);
+      captureNode.connect(audioCtx.destination);
 
-			audioCtx.resume();
+      audioCtx.resume();
 
-			set({ audioCtx, captureNode, isInit: true });
-		},
-		record: () => {
-			const captureNode = get().captureNode;
-			if (captureNode) {
-				captureNode.port.postMessage({ cmd: "rec" });
-			}
-		},
-		playNote: (midiNote: number) => {
-			const captureNode = get().captureNode;
-			if (captureNode) {
-				captureNode.port.postMessage({ cmd: "play", val: midiNote });
-			}
-		},
-		stopNote: (midiNote: number) => {
-			const captureNode = get().captureNode;
-			if (captureNode) {
-				captureNode.port.postMessage({ cmd: "stop", val: midiNote });
-			}
-		},
-	};
+      set({ audioCtx, captureNode, isInit: true });
+    },
+    record: () => {
+      const captureNode = get().captureNode;
+      if (captureNode) {
+        captureNode.port.postMessage({ cmd: "rec" });
+      }
+    },
+    playNote: (midiNote: number) => {
+      const captureNode = get().captureNode;
+      if (captureNode) {
+        captureNode.port.postMessage({ cmd: "play", val: midiNote });
+      }
+    },
+    stopNote: (midiNote: number) => {
+      const captureNode = get().captureNode;
+      if (captureNode) {
+        captureNode.port.postMessage({ cmd: "stop", val: midiNote });
+      }
+    },
+  };
 });
